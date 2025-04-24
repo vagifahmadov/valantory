1c55dc92b61729ca77c97d6f248c81936e84474e
Vagif <np.info.mail@gmail.com>
2025-04-24 06:23:57 +0400

    24.04.2025

--- faceid/main/views.py
+++ faceid/main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 def home_page(r):
     return render(r, "index.html")
 
 def recognition_page(r):
+    request_type = str(str(list(r)[0]).split("&")[1]).split('=')[1].replace("'", "")
+    print(f'\n\n\n\n===================\n{request_type}\n---------------------\n\n\n\n')
     return render(r, "recognition.html")
