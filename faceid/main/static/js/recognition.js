9c75ee267a8e4105163263e315978602659ebeac
Vagif <np.info.mail@gmail.com>
2025-04-29 20:46:04 +0400

    29.04.2025

--- faceid/main/static/js/recognition.js
+++ faceid/main/static/js/recognition.js
@@ -14,6 +14,9 @@ function countdown() {
         if (seconds < 10){
             counter.classList.add("blink_timeout");
             counter.style.color = '#ce4975';
+        }else{
+            counter.classList.remove("blink_timeout");
+            counter.style.color = '#042B8C';
         }
         // console.log(`time is:`+seconds);
       } else {
@@ -23,39 +26,8 @@ function countdown() {
     }
     tick();
   }
-  countdown();
-// swiftalert
-  const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-      confirmButton: "btn btn-success",
-      cancelButton: "btn btn-danger"
-    },
-    buttonsStyling: false
-  });
-  swalWithBootstrapButtons.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonText: "Yes, delete it!",
-    cancelButtonText: "No, cancel!",
-    reverseButtons: true
-  }).then((result) => {
-    if (result.isConfirmed) {
-      swalWithBootstrapButtons.fire({
-        title: "Deleted!",
-        text: "Your file has been deleted.",
-        icon: "success"
-      });
-    } else if (
-      /* Read more about handling dismissals below */
-      result.dismiss === Swal.DismissReason.cancel
-    ) {
-      swalWithBootstrapButtons.fire({
-        title: "Cancelled",
-        text: "Your imaginary file is safe :)",
-        icon: "error"
-      });
-    }
-  });
+
+countdown();
+
+
   
