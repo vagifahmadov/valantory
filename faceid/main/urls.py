158962dd08eaf5f54eb4e30de78cb4e3c7b56d98
Vaqif Əhmədov <v.ahmedov@asan.gov.az>
2025-05-13 18:01:06 +0400

    13.05.2024

--- faceid/main/urls.py
+++ faceid/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from tkinter.font import names
 
-from .views import home_page, recognition_page, capture_service
+from .views import home_page, recognition_page, capture_service, pin_service
 
 urlpatterns = [
     path("", home_page, name="home"),
     path("recognition", recognition_page, name="recognition"),
     path("capture", capture_service, name="capture"),
+    path("pin", pin_service, name="pin", )
 ]
--- faceid/main/views.py
+++ faceid/main/views.py
@@ -5,15 +5,18 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 
+
 # Create your views here.
 def home_page(r):
     return render(r, "index.html")
+
 
 def recognition_page(r):
     # print(list(r))
     # request_type = str(str(list(r)[0]).split("&")[1]).split('=')[1].replace("'", "")
     # print(f'\n\n\n\n===================\n{request_type}\n---------------------\n\n\n\n')
     return render(r, "recognition.html")
+
 
 @csrf_exempt
 def capture_service(r):
@@ -24,5 +27,17 @@ def capture_service(r):
         ip_addr = data.get('ip')
         print(f'\n\n\n\n===================\n{data}\n---------------------\n\n\n\n')
         return JsonResponse({'received': True, 'key1': b64_img, 'key2': ip_addr})
+    else:
+        return None
+
+
+@csrf_exempt
+def pin_service(r):
+    if r.method == 'POST':
+        print('pin posted\n')
+        data = json.loads(r.body)
+        b64_img = data.get('b64img')
+        pin = data.get('pin')
+        return JsonResponse({'received': True, 'key1': b64_img, 'key2': pin})
     else:
         return None
